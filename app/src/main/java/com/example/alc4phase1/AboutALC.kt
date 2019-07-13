249a35a6b3b140e3501eb2bead50dbba857f4b81
ibnahmad <justforlearningcode@gmail.com>
2019-07-13 16:40:58 +0100

    Added About ALC Activity

---
+++ app/src/main/java/com/example/alc4phase1/AboutALC.kt
@@ -0,0 +1,12 @@
+package com.example.alc4phase1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutALC : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_alc)
+    }
+}
